0f6ba85caf6fa1f781d37b5c177f40d733b9d7f8
James Dalessio <jrd261@gmail.com>
2016-05-22 08:30:05 -0400

    Remove console logs and update to latest player syntax.

--- bin/exp.js
+++ bin/exp.js
@@ -4,19 +4,17 @@ var commander = require('commander');
 var path = require('path');
 var devPlayer = require('../lib/dev-player');
 
-commander.version('0.0.0')
+commander.version('0.0.3')
 
 commander.command('play [path]')
   .description('launch a player app in your local browser on a player')
   .option('-p, --port [port]', 'the port to run on (8899)', parseInt)
   .option('-e, --host [host]', 'the player host (https://player.goexp.io)')
   .action(function (path_, env) {
-    console.log(env);
     var options = {};
     options.path = path.resolve(path_ || './');
     options.host = env.host || 'https://player.goexp.io';
     options.port = env.port || 8899;
-    console.log(options);
     devPlayer.start(options);
   });
 

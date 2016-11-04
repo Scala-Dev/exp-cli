2a8d360b079143e852d768601b28828ab3fd4b99
Adam Galloway <adam.galloway@scala.com>
2016-11-04 13:53:14 -0400

    app deploy wip EXP-2270

--- bin/exp.js
+++ bin/exp.js
@@ -5,6 +5,7 @@ var path = require('path');
 var devPlayer = require('../lib/dev-player');
 var init = require('../lib/init');
 var deploy = require('../lib/deploy');
+var auth = require('../lib/auth');
 
 commander.version('0.0.5')
 
@@ -26,7 +27,18 @@ commander.command('init')
 
 commander.command('deploy')
   .description('deploy a player app to your organization')
-  .action(deploy);
+  .option('-a, --app [app]', 'the remote path of the app to deploy')
+  .option('-h, --host [host]', 'the api host (https://api.goexp.io)')
+  .action(function (env) {
+    var options = {};
+    options.host = env.host || 'https://api.goexp.io';
+    options.app = env.app;
+    deploy(options);
+  });
+
+commander.command('logout')
+  .description('remove any temporary credentials')
+  .action(auth.logout);
 
 commander.command('*', '',  { isDefault: true, noHelp: true })
   .action(function () {
@@ -35,6 +47,6 @@ commander.command('*', '',  { isDefault: true, noHelp: true })
 
 commander.parse(process.argv);
 
-if (!process.argv.slice(2).length) {  
+if (!process.argv.slice(2).length) {
   commander.help();
 }
